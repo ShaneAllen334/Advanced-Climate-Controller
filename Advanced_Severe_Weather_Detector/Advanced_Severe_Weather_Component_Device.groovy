8884867dc043250f83feab6eaae64311006ee29b
Shane Allen <shaneschwarzman@protonmail.com>
2026-04-02 17:27:45 -0500

    Update Advanced_Severe_Weather_Component_Device.groovy

--- Advanced_Severe_Weather_Detector/Advanced_Severe_Weather_Component_Device.groovy
+++ Advanced_Severe_Weather_Detector/Advanced_Severe_Weather_Component_Device.groovy
@@ -1,70 +1,81 @@
 /**
- * Advanced Severe Weather Component Device
+ * Advanced Rain Detector Information Device
  */
+
 metadata {
-    definition(name: "Advanced Severe Weather Device", namespace: "ShaneAllen", author: "ShaneAllen", component: true) {
+    definition (
+        name: "Advanced Rain Detector Information Device", 
+        namespace: "ShaneAllen", 
+        author: "ShaneAllen",
+        description: "Virtual device that holds advanced meteorological calculations from the Advanced Rain Detection app."
+    ) {
         capability "Sensor"
-        capability "Actuator"
+        capability "WaterSensor" // Enables native Hubitat Safety Monitor (HSM) Wet/Dry integration
         
-        // Global States
-        attribute "globalThreatState", "string"
+        // Core States
+        attribute "weatherState", "string"
+        attribute "rainProbability", "number"
+        attribute "confidenceScore", "number"
+        attribute "expectedClearTime", "string"
+        
+        // Evaporation & Drying
+        attribute "dryingPotential", "string"
+        attribute "timeToDry", "string"
+        
+        // Precipitation States
+        attribute "sprinkling", "string"
+        attribute "raining", "string"
+        
+        // Calculated Thermodynamics
+        attribute "vpd", "number"
+        attribute "wetBulb", "number"
+        attribute "dewPoint", "number"
+        attribute "dewPointSpread", "number"
+        
+        // Trends, Velocities & Advanced Metrics
         attribute "pressureTrend", "string"
         attribute "tempTrend", "string"
+        attribute "spreadTrend", "string"
+        attribute "luxTrend", "string"
         attribute "windTrend", "string"
+        attribute "windDirection", "number"
+        attribute "windShiftDetected", "string"
+        
+        // Lightning Metrics
+        attribute "lightningStrikeCount", "number"
+        attribute "lightningClosestDistance", "number"
+        
+        // Accumulation History
         attribute "currentDayRain", "number"
-
-        // NOAA Forecast
-        attribute "noaaForecastText", "string"
-        attribute "noaaForecastTile", "string"
-
-        // Tornado
-        attribute "tornadoThreat", "number"
-        attribute "tornadoProb", "number"
-        attribute "tornadoConf", "number"
-        attribute "tornadoState", "string"
-        attribute "tornadoTile", "string"
-
-        // Thunderstorm
-        attribute "thunderstormThreat", "number"
-        attribute "thunderstormProb", "number"
-        attribute "thunderstormConf", "number"
-        attribute "thunderstormState", "string"
-        attribute "thunderstormTile", "string"
-
-        // Flood
-        attribute "floodThreat", "number"
-        attribute "floodProb", "number"
-        attribute "floodConf", "number"
-        attribute "floodState", "string"
-        attribute "floodTile", "string"
-
-        // Freeze
-        attribute "freezeThreat", "number"
-        attribute "freezeProb", "number"
-        attribute "freezeConf", "number"
-        attribute "freezeState", "string"
-        attribute "freezeTile", "string"
-
-        // Severe Heat
-        attribute "severeheatThreat", "number"
-        attribute "severeheatProb", "number"
-        attribute "severeheatConf", "number"
-        attribute "severeheatState", "string"
-        attribute "severeheatTile", "string"
-
-        // Tropical
-        attribute "tropicalThreat", "number"
-        attribute "tropicalProb", "number"
-        attribute "tropicalConf", "number"
-        attribute "tropicalState", "string"
-        attribute "tropicalTile", "string"
+        attribute "recordRainAmount", "number"
+        attribute "recordRainDate", "string"
     }
 }
 
 def installed() {
-    log.info "Severe Weather Child Device Installed"
+    log.info "Advanced Rain Detector Information Device Installed"
+    sendEvent(name: "water", value: "dry")
 }
 
 def updated() {
-    log.info "Severe Weather Child Device Updated"
+    log.info "Advanced Rain Detector Information Device Updated"
+}
+
+// Intercept incoming events from the App to automatically manage the native WaterSensor capability
+void sendEvent(Map properties) {
+    // Automatically manage the HSM wet/dry state based on the engine's weatherState
+    if (properties.name == "weatherState") {
+        if (properties.value == "Raining" || properties.value == "Sprinkling") {
+            if (device.currentValue("water") != "wet") {
+                super.sendEvent([name: "water", value: "wet", descriptionText: "Precipitation active, reporting WET to HSM."])
+            }
+        } else if (properties.value == "Clear") {
+            if (device.currentValue("water") != "dry") {
+                super.sendEvent([name: "water", value: "dry", descriptionText: "Precipitation clear, reporting DRY to HSM."])
+            }
+        }
+    }
+    
+    // Pass the standard event through
+    super.sendEvent(properties)
 }
